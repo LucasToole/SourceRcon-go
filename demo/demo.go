86daeebbc29a8fbcf7d5aefa6c0ed9e3d8a7ffa7
Lucas Toole <lucas@ltoole.com>
2020-02-21 19:22:09 -0600

    Initial Commit

---
+++ demo/demo.go
@@ -0,0 +1,13 @@
+/* Copyright (c) 2020 Lucas Toole. See LICENSE for details */
+
+package main
+
+import (
+	"fmt"
+	"../rcon"
+)
+
+func main() {
+	fmt.Println("In main()")
+	rcon.Test()
+}
---
+++ rcon/rcon.go
@@ -0,0 +1,11 @@
+/* Copyright (c) 2020 Lucas Toole. See LICENSE for details */
+
+package rcon
+
+import (
+	"fmt"
+)
+
+func Test() {
+	fmt.Println("In Test()")
+}
